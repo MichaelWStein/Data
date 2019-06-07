c7da63d9fd4b117e3f7aa7ea21935d2de1650015
Hugo Azevedo <hrpitta@gmail.com>
2019-06-07 15:42:41 -0400

    no message

--- ValueofVote_App/static/js/app_ed.js
+++ ValueofVote_App/static/js/app_ed.js
@@ -1,6 +1,6 @@
 // Creating map object
 
-
+  var mapboxAccessToken = "{your access token here}";
   var map = L.map("map-ed", {
     center: [57.130367, -106.346771],
     zoom: 3,
@@ -8,11 +8,10 @@
 
   
   // Adding tile layer
-  lightMap=L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  lightMap=L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken, {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
+    id: "mapbox.light",
   }).addTo(map);
   
   var link = "/ed_geojson";
